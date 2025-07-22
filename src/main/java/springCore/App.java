5762b60a8678f6fe0782a21355c94b8342665719
aniket234121 <aniketchauhan329@gmail.com>
2025-07-22 21:44:59 +0530

    setup initial

---
+++ src/main/java/springCore/App.java
@@ -0,0 +1,13 @@
+package springCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
